607e8b3842a3ef22acd4fe9476e784bd8221f0b3
parthteredesai <parth.teredesai@gmail.com>
2026-09-16 17:33:19 +0530

    setup3

---
+++ frontend/src/landing_page/about/AboutHero.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AboutHero() {
+    return ( 
+        <h1>hero</h1>
+     );
+}
+
+export default AboutHero;
---
+++ frontend/src/landing_page/about/AboutPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AboutHero from './AboutHero';
+import Team from './Team';
+
+function AboutPage() {
+    return ( 
+        <>
+        <AboutHero />
+        <Team />
+        </>
+     );
+}
+
+export default AboutPage;
